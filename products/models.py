a16f1f234b8a561677e9b65e7a3ffbd53dc010c4
Kelvin Adjei <kelvinaf122@gmail.com>
2024-10-20 20:34:30 +0000

    initial

---
+++ products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock_quantity = models.IntegerField()
+    image_url = models.URLField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
---
+++ products/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
